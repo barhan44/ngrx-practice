837ad40f211ae65bf1d65ab5d2f4707e7cc82c75
sergey.gunyashov <gunyashov44@gmail.com>
2020-05-09 14:57:20 +0300

    Add: routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: 'notes',
+    loadChildren: () => import('./modules/notes/notes.module').then(m => m.NotesModule)
+  },
+  {
+    path: 'todos',
+    loadChildren: () => import('./modules/todos/todos.module').then(m => m.TodosModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
