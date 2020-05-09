a0c09d956513312df95aeacbc0d2edb6f767bfa0
sergey.gunyashov <gunyashov44@gmail.com>
2020-05-09 15:46:42 +0300

    Add: generated notes components

--- src/app/modules/notes/notes-routing.module.ts
+++ src/app/modules/notes/notes-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotePageComponent } from "./note-page/note-page.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: NotePageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/notes/notes.module.ts
+++ src/app/modules/notes/notes.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NotesRoutingModule } from './notes-routing.module';
+import { NoteFormComponent } from './note-form/note-form.component';
+import { NoteListComponent } from './note-list/note-list.component';
+import { NotePageComponent } from './note-page/note-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NoteFormComponent, NoteListComponent, NotePageComponent],
   imports: [
     CommonModule,
     NotesRoutingModule
