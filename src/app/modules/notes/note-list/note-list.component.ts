2ddfc9373418dfcc4a42bd449f27e21693ba0168
barhan44 <gunyashov44@gmail.com>
2020-05-11 00:42:21 +0300

    Add: update note functionality

--- src/app/modules/notes/note-list/note-list.component.ts
+++ src/app/modules/notes/note-list/note-list.component.ts
@@ -1,19 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Note } from '../model/note.class';
 import { NoteService } from '../note.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-note-list',
   templateUrl: './note-list.component.html',
   styleUrls: ['./note-list.component.scss']
 })
-export class NoteListComponent {
+export class NoteListComponent implements OnInit {
 
   @Input() note: Note;
+
+  noteEditForm: FormGroup;
+
+  editMode = false;
 
   constructor(
     private noteService: NoteService
   ) {
+  }
+
+  ngOnInit(): void {
+    this.initEditForm();
+  }
+
+  private initEditForm() {
+    this.noteEditForm = new FormGroup({
+      title: new FormControl(this.note.title, Validators.required),
+      text: new FormControl(this.note.text, Validators.required)
+    });
   }
 
   onNoteDelete(): void {
@@ -21,6 +37,18 @@ export class NoteListComponent {
   }
 
   onNoteEdit() {
-    this.noteService.updateNote(this.note);
+    this.editMode = true;
+  }
+
+  onCancelEdit() {
+    this.editMode = false;
+    this.initEditForm();
+  }
+
+  onSaveEdit() {
+    const noteTitle = this.noteEditForm.get('title')?.value;
+    const noteText = this.noteEditForm.get('text')?.value;
+    const note = new Note(noteTitle, noteText, this.note.id);
+    this.noteService.updateNote(note);
   }
 }
