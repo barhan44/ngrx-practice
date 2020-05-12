bd71beb04eaa55df19eb4fbe8147a55557b5ffcd
barhan44 <gunyashov44@gmail.com>
2020-05-12 16:20:02 +0300

    Delete: needless module

--- src/app/modules/notes/notes.module.ts
+++ src/app/modules/notes/notes.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { NotesRoutingModule } from './notes-routing.module';
 import { NoteFormComponent } from './note-form/note-form.component';
@@ -15,7 +15,6 @@ import { NoteService } from './note.service';
     CommonModule,
     NotesRoutingModule,
     HttpClientModule,
-    FormsModule,
     ReactiveFormsModule,
   ],
   providers: [NoteService],
