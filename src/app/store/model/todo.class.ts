8cda651bdcf07f6b9b73e6ec0c976e8ccea08a9d
barhan44 <gunyashov44@gmail.com>
2020-05-12 16:13:31 +0300

    Change: default value

--- src/app/store/model/todo.class.ts
+++ src/app/store/model/todo.class.ts
@@ -2,7 +2,7 @@ export class Todo {
   constructor(
     public title: string,
     public text: string,
-    public completed: boolean,
+    public completed: boolean = false,
     public id?: number
   ) {}
 }
